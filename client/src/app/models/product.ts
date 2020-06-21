0bc78d4e4f55a9bcea1b634bc08ffcdcc718f224
GalantKac <kacper21galant@gmail.com>
2020-06-21 19:29:59 +0200

    Edited UI nav-bar, add model ts classes

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    picturePath: string;
+    productBrand: string;
+    productType: string;
+  };
