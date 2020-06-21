53efad499be7056b10697ae626b66d201162bf73
GalantKac <kacper21galant@gmail.com>
2020-06-21 20:27:32 +0200

    Craeted client structure. Created core module with service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IProduct } from './models/product';
-import {IPagination} from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +9,10 @@ import {IPagination} from './models/pagination';
 export class AppComponent implements OnInit {
   //pole
   title = 'E_Commerce';
-  products: IProduct[];
 
   constructor(private http: HttpClient) { }
 
   //metody interfejsu
   ngOnInit(): void {
-    this.http.get('https://localhost:44349/api/TProducts?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
   }
 }
