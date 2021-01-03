a7ec86f24e23f17a43b5853b5aaa03c9c13402d8
Bartosz Galantowicz <galantowiczbartosz@gmail.com>
2021-01-03 17:11:17 +0100

    Working on register form

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { AccountService } from './../account.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();  
   }
 
+  createRegisterForm(){
+    this.registerForm = this.fb.group({
+      displayName: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.pattern('^[\\w-\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
+      password: [null, [Validators.required]]
+    });
+  }
 }
